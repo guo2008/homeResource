b52f435f0c9c066e9328a1cfa7dd6cc1fb7f2617
guo2008 <guo2008@gmail.com>
2017-09-04 18:31:30 -0400

    init commit

---
+++ src/main/java/org/martin/chapter1/HelloServlet.java
@@ -0,0 +1,28 @@
+package org.martin.chapter1;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by fguo on 2017-09-04.
+ */
+@WebServlet("hello/")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+		    throws ServletException, IOException {
+
+	DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	String currentTime = df.format(new Date());
+	req.setAttribute("currenttime", currentTime);
+	req.getRequestDispatcher("WEB-INF/jsp/hello.jsp").forward(req, resp);
+    }
+}
---
+++ src/main/webapp/jsp/hello.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: fguo
+  Date: 2017-09-04
+  Time: 6:20 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Hello</title>
+</head>
+<body>
+    <h1>hello!</h1>
+    <h2>current time: ${curremttime}</h2>
+</body>
+</html>
